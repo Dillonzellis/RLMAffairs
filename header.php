f28f59e7900331359aa31b6253981bcffa2e1af5
Dillon Ellis <dillonzellis@gmail.com>
2021-04-06 16:22:11 -0400

    navbar

--- header.php
+++ header.php
@@ -14,6 +14,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -22,13 +23,31 @@ $container = get_theme_mod( 'understrap_container_type' );
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
-
-	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar">
-
-
-
-
-	</div><!-- #wrapper-navbar end -->
+	<?php do_action( 'wp_body_open' ); ?>
+	<div class="site" id="page">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">RLM Affairs Logo</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Event Planner<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Venue</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">RLM Florist</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Catering Menu</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Babycakes</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
