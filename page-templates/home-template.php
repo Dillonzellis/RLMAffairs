e93285f294ac22f67fa985b459688b91b97f1fd6
Dillon Ellis <dillonzellis@gmail.com>
2021-04-06 17:46:01 -0400

    landing page html/sass

--- page-templates/home-template.php
+++ page-templates/home-template.php
@@ -12,37 +12,63 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
 
-    <p>
-        Floral Showroom:  RLM Affairs  is a full-service florist serving North  Georgia for 40 years. Our award winning designers offer exquisite attention to detail. Whether it’s a single rose or decor for an entire Gala every bloom is hand picked and sourced eco friendly. Our retail storefront offers modern & unique items keeping everyone in mind
-    </p>
+<div class="container">
+	<div class="row">
+		<div class="col-1">
+            logo
+		</div>
+		<div class="col">
+			<p>
+				Floral Showroom: RLM Affairs is a full-service florist serving North Georgia for 40 years. Our award
+				winning
+				designers offer exquisite attention to detail. Whether it’s a single rose or decor for an entire Gala
+				every
+				bloom is hand picked and sourced eco friendly. Our retail storefront offers modern & unique items
+				keeping
+				everyone in mind
+			</p>
+		</div>
+
+	</div>
+
+</div>
 
 
-    <button>
-        ORDER FLOWERS NOW
-    </button>
 
-    <p>
-        Event Planner: At RLM Affairs we capture your vision and create the memories you have dreamed. Whether these dreams include something you have believed would be your day since you were a child, or an event you were asked to coordinate a week ago. Our goal is to elegantly bring your vision to life.  Our amazing team has 40 years of industry experience guaranteeing a flawless execution.
-    </p>
+<button>
+	ORDER FLOWERS NOW
+</button>
 
-    <p>
-        We have had many different influences in the floral industry, affording us the opportunity to create something unique. We source our flowers direct from the grower & import any flower anytime of the year we can provide any floral desires.
-    </p>
+<p>
+	Event Planner: At RLM Affairs we capture your vision and create the memories you have dreamed. Whether these dreams
+	include something you have believed would be your day since you were a child, or an event you were asked to
+	coordinate a week ago. Our goal is to elegantly bring your vision to life. Our amazing team has 40 years of industry
+	experience guaranteeing a flawless execution.
+</p>
 
-    <p>
-        We plan, design and fully execute beautiful celebrations of any size. We love creating events that will make our clients and their guest dance the night away and create memories that
-    </p>
+<p>
+	We have had many different influences in the floral industry, affording us the opportunity to create something
+	unique. We source our flowers direct from the grower & import any flower anytime of the year we can provide any
+	floral desires.
+</p>
 
-    <button>
-        CONSULTATION FORM
-    </button>
+<p>
+	We plan, design and fully execute beautiful celebrations of any size. We love creating events that will make our
+	clients and their guest dance the night away and create memories that
+</p>
 
-    <p>
-        BabyCakes specializes in hand crafted cupcakes. We make everything fresh from scratch in small batches throughout the day using the finest ingredients, including sweet cream butter, Vanilla bean, bittersweet Belgian chocolate, fresh zucchini and carrots.
-    </p>
+<button>
+	CONSULTATION FORM
+</button>
 
-    <button>
-        ORDER BABYCAKES NOW
-    </button>
+<p>
+	BabyCakes specializes in hand crafted cupcakes. We make everything fresh from scratch in small batches throughout
+	the day using the finest ingredients, including sweet cream butter, Vanilla bean, bittersweet Belgian chocolate,
+	fresh zucchini and carrots.
+</p>
+
+<button>
+	ORDER BABYCAKES NOW
+</button>
 
 <?php get_footer();
