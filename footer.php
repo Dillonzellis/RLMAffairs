d87cfa45ab72448f2815423646c16c594d9c3449
Dillon Ellis <dillonzellis@gmail.com>
2021-04-06 16:49:54 -0400

    landing page html

--- footer.php
+++ footer.php
@@ -27,7 +27,21 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						<div class="div">
+							RLM AFFAIRS 3575 McEver Road, Gainesville, GA, 30504, United States 770-536-7306 Resia@RLMAffairs.com
+						</div>
+
+						<div class="div">
+							Hours Mon 10:00-4:00 Tue 10:00-4:00 Wed 10:00-4:00 Thu 10:00-4:00 Fri 10:00-4:00 Sat 10:00-12:00
+						</div>
+
+						<div class="div">
+							best of knot
+						</div>
+
+						<div class="div">
+							SOCIAL LINKS
+						</div>
 
 					</div><!-- .site-info -->
 
