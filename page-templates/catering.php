e1ded4543210ec9c99a5db8807aaf9b35f0e3f5d
Dillon Ellis <dillonzellis@gmail.com>
2021-07-05 22:42:10 -0400

    pics/rough venue/catering/babycakes pages

---
+++ page-templates/catering.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Catering Template
+ *
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header('clean');
+?>
+
+<div class="container">
+    <div class="row mb-5">
+        <p>
+            RLM Affairs offers all the services you may need for your event. Our staff works directly with our clients to tailor each event to your specific needs. Whether you need just catering, or a complete event design, we will work with you as much as you need, or we will provide you with A la carte services based specifically on your needs and budget. Our award winning catering, wedding planning, cakes, and floral design are available every day, breakfast, lunch, dinner, or in between. 
+        </p>
+    </div>
+</div>
+
+<?php get_footer();
--- page-templates/venue.php
+++ page-templates/venue.php
@@ -25,8 +25,269 @@ get_header('clean');
     
 </div>
 
-    <hr class="mb-5">
+<hr class="mb-5">
 
+<div class="container">
+    <div class="row align-items-center mb-5">
+        <div class="col-2">
+            <img class="logo"src="<?php echo get_template_directory_uri(); ?>/img/venue-rlm-logo.jpg" alt="" srcset="">
+        </div>
+        
+        <div class="col-10">
+            <p>
+                RLM Affairs offers all the services you may need for your event. Our staff works directly with our clients to tailor each event to your specific needs. Whether you need just catering, or a complete event design, we will work with you as much as you need, or we will provide you with A la carte services based specifically on your needs and budget. Our award winning catering, wedding planning, cakes, and floral design are available every day, breakfast, lunch, dinner, or in between.
+            </p>
+        </div>
+
+    </div>
+</div>
+
+<hr class="mb-5">
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>Estate Room & Gardens Packages:</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            Sapphire Wedding Package: $3300.00 
+        </div>
+    </div>
+
+    <ul>
+        <li>65 Guests</li>
+        <li>7 hour Venue Rental Included</li>
+        <li>Reception Décor: Centerpieces flowers for food display & reception</li>
+        <li>Ceremony Décor: organza draping</li>
+        <li>Wedding Party: flower girl petals Bride bouquet & 3 bridesmaids Groom & 3 groomsmen boutonnières</li>
+        <li>Set Up & display of couples personal items</li>
+        <li>1 entrée buffet with 2 side dishes and bread Or Heavy Hors d’Oeuvres with 3 choices</li>
+        <li>Tea and Water beverages</li>
+        <li>Disposable Flatware Glasses and Paper Napkins</li>
+        <li>2 tier Cake with Cake stand, Staff will cut & serve cake Vanilla or Chocolate, Rough, Smooth or Naked iced</li>
+        <li>Staff for event 4 hours</li>
+        <li>Rehearsal Day of Event</li>
+        <li>Monday- Thursday: $2900.00</li>
+    </ul>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Emerald Wedding Package: $4500.00
+        </div>
+    </div>
+
+    <ul>
+        <li>70 Guests</li>
+        <li>8 hour Venue Rental Included</li>
+        <li>Reception Décor: Centerpieces flowers for food display & reception</li>
+        <li>Ceremony Décor: Organza draping with optional florals & Aisle décor</li>
+        <li>Bride bouquet & 3 bridesmaids and flower girl petals Groom & 3 groomsmen boutonnières</li>
+        <li>Cocktail hour 2 Hors d’Oeuvres</li>
+        <li>1 entrée buffet, salad, rolls, 2 side dishes</li>
+        <li>Tea and water beverages With display Decanters</li>
+        <li>Silver flatware glasses and plates with signature paper napkins  </li>
+        <li>2 tier cake with Cake stand Staff will cut & serve cake Vanilla or Chocolate, Rough, Smooth or Naked iced</li>
+        <li>Staff for event 4 hours</li>
+        <li>Wedding Ceremony Coordination/Dinner service/ Cake Cutting</li>
+        <li>Rehearsal Day of Event</li>
+        <li>Monday- Thursday: $4000.00</li>
+    </ul>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Ruby Wedding Package: $6400.00
+        </div>
+    </div>
+
+    <ul>
+        <li>70 Guests</li>
+        <li>10 hour Venue Rental Included</li>
+        <li>Ceremony Décor: Organza draping with Full Florals & with aisle pew decor</li>
+        <li>Reception Décor: Full floral Centerpieces flowers for food display & reception </li>
+        <li>Bride bouquet & 3 bridesmaids and flower girl petals Groom & 3 groomsmen boutonnières </li>
+        <li>Catering: Cocktail hour 3 Hors d’Oeuvres 2 entrée buffet, signature salad, diner rolls, 2 side dishes </li>
+        <li>Beverages Displayed in Elegant Decanters: Tea and water beverages </li>
+        <li>Silver or Gold flatware, Goblets and Dinner Plates with Linen Napkins White or cream linens for seating tables </li>
+        <li>3 tier cake with Cake stand, staff will cut & serve cake Vanilla or Chocolate, Rough, Smooth or Naked iced</li>
+        <li>Photo Booth Wall</li>
+        <li>Staff for event 6 hours</li>
+        <li>Day of Coordinator Wedding Ceremony Coordination/Dinner service/ Cake Cutting</li>
+        <li>Rehearsal with coordinator </li>
+        <li>Clean up</li>
+        <li>Monday- Thursday: $5900.00</li>
+    </ul>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Diamond  Wedding Package: $8400.00
+        </div>
+    </div>
+
+    <ul>
+        <li>75 Guests</li>
+        <li>12 hour Venue Rental Included</li>
+        <li>Ceremony Décor: Organza draping with Full Florals & with aisle pew decor</li>
+        <li>Reception Décor: Full floral Centerpieces flowers for food display & reception </li>
+        <li>Bride bouquet & 5 bridesmaids and flower girl petals Groom & 5 groomsmen boutonnières </li>
+        <li>3 Corsages (Moms Grandmothers Etc.) 3 Boutioneres (Fathers Grandfathers Etc)</li>
+        <li>Catering: Cocktail hour 3 Hors d’Oeuvres 2 entrée buffet, signature salad, diner rolls, 2 side dishes </li>
+        <li>Beverages Displayed in Elegant Decanters: Tea and water beverages </li>
+        <li>Silver or Gold flatware, Goblets and Dinner Plates with Linen Napkins. White or cream linens for seating tables </li>
+        <li>3 tier cake with Cake stand, staff will cut & serve cake Vanilla or Chocolate, Rough, Smooth or Naked iced</li>
+        <li>Photo Booth Wall</li>
+        <li>Staff for event 6 hours</li>
+        <li>Day of Coordinator Wedding Ceremony Coordination/Dinner service/ Cake Cutting</li>
+        <li>Rehearsal with coordinator </li>
+        <li>Full set up & Clean up</li>
+        <li>Monday- Thursday: $7900.00</li>
+    </ul>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Reception Only: $2700.00
+        </div>
+    </div>
+
+    <ul>
+        <li>70 Guests</li>
+        <li>Venue Rental Included 4 hours</li>
+        <li>Reception tables & chairs</li>
+        <li>1 Entree 2 sides dinner roll & salad</li>
+        <li>Set Up & clean up</li>
+        <li>Beverages Displayed in Decanters: Tea and water beverages </li>
+        <li>Disposable service with signature paper napkins</li>
+        <li>White or cream linens for seating tables</li>
+        <li>Clean up dining area and kitchen</li>
+        <li>2 tier cake with Cake stand Vanilla or Chocolate, Rough, Smooth or Naked iced</li>
+        <li>Staff for event 4 hours</li>
+    </ul>
+</div>
+
+<div class="container">
+
+    <h3>Rental Fees:</h3>
+
+    <p>Photo Booth Wall $300.00</p>
+
+    <p>Corn Hole, Giant Jenga $10.00 each per event</p>
+
+    <p>Champagne Wall $1.50 a glass</p>
+
+    <p>Sparkler Buckets $4.00 each</p>
+
+    <p>Bar Glassware $16 a rack</p>
+
+    <p>Extra Decanters $8.00 each</p>
+
+    <p>Tasting $75.00</p>
+
+    <p>Fridays, Saturdays & Sundays - $1800.00 10 hours
+    Monday-Thursday - $1,200 6 hours /Hourly rental 250.00 an hour
+    A $500 deposit is required to hold a date. This deposit is not refundable.</p>
+
+    <h3>Venue Features</h3>
+
+    <ul>
+        <li>Outdoor Pew seating / Reception seating</li>
+        <li>Private Lounge & Dressing rooms with private bath and separate entrance</li>
+        <li>Built in Food Service Bar</li>
+        <li>Built in Bar</li>
+        <li>Full kitchen</li>
+        <li>Outdoor patio space with outdoor seating areas, fairy & Café lights</li>
+        <li>Audio system throughout & Microphone</li>
+        <li>Dance Floor</li>
+        <li>Vintage Estate wood Tables & folding chairs</li>
+    </ul>
+
+    <h3>Preferred Vendors:</h3>
+
+    <h4>Photography:</h4>
+
+    <p>Rita Wedding Studio https://ritaweddingstudio</p>
+
+    <p>Far Out Galaxy - Photography - 770-876-9248</p>
+
+    <p>Far Out Galaxy - Cinematography - 770-876-9248</p>
+
+    <p>Cheri Michelle Photography</p>
+
+    <br>
+
+    <h3>DJ Services:</h3>
+
+    <p>Far Out Galaxy - (DJ) - 770-876-9248</p>
+
+    <h3>Officiant:</h3>
+
+    <p>Rev. Heinz Kloeter - 678-983-7976</p>
+
+    <p>Alternative Weddings  - Rev. Dr. Barbara Roberts - 706-265-7794</p>
+
+    <p>Rev. Gale York - 678-890-4881</p>
+
+    <p>Davis Palmour - 706-455-8421</p>
+
+    <p>Ceremonies By Rick - Rick Durham - 706-654-6268</p>
+
+    <p>Tyler Maraia - 206-245-5986</p>
+
+    <p>Sensational Ceremonies - Dr. Gregg Potts - 678-205-7267</p>
+
+    <p>Sue Palmer (minister) - 470-232-1170</p>
+
+    <p>Rocky Thompason - 770-490-1274</p>
+
+    <br>
+
+    <h3>Hair & Make UP</h3>
+
+    <p>Bliss Formal Hair - Candice Miklius</p>
+
+    <p>Mind's Eye Salon - 678-828-7747</p>
+
+    <h3>Event Travel & Insurance</h3>
+
+    <p>Vacations by Sally - Sally Pavao - 706-360-1346</p>
+
+    <p>VIP Southern Tours  - 706-348-8747</p>
+
+    <p>Bradford Turner Insurance Group - Brittany Turner - Wedding Rate Chart - Wedding Quote</p>
+
+    <h3>Bartending:</h3>
+
+    <p>Kim's Cocktails - 404-273-5902 -  Kim Pickerel</p>
+</div>
+
+<div class="container photos">
+
+    <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+
+</div>
+
+<div class="container">
+    <?php
+        while ( have_posts() ) {
+            the_post();
+            get_template_part( 'loop-templates/content', 'page' );
+
+
+        }
+    ?>
+</div>
 
 
 
